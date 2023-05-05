d689f7b2ec6c9716012e66d0cbb08c0684f294d3
Shahghasi Adil <shaghay555@gmail.com>
2023-05-05 10:41:40 +0430

    role utils created

---
+++ utils/role.js
@@ -0,0 +1,16 @@
+const ROLE = {
+    ADMIN: 'admin',
+    INSTRUCTOR: 'instructor',
+    STUDENT: 'student',
+  };
+  
+  const authorize = (...roles) => {
+    return (req, res, next) => {
+      if (!roles.includes(req.user.role)) {
+        return res.status(403).send({ error: 'Access denied.' });
+      }
+      next();
+    };
+  };
+  
+  module.exports = { ROLE, authorize };
