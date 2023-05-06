5602fda35de8bb60ea23c92647685a82f7bd790c
Shahghasi Adil <shaghay555@gmail.com>
2023-05-06 13:43:25 +0430

    badges routes added

---
+++ routes/badge.routes.js
@@ -0,0 +1,19 @@
+// routes/badgeRoutes.js
+const express = require("express");
+const badgeController = require("../controllers/badge.controller");
+const auth = require("../middlewares/auth");
+const authorize = require("../utils/role");
+
+const router = express.Router();
+
+router.post("/", auth, authorize("admin"), badgeController.createBadge);
+router.get("/", auth, authorize("admin"), badgeController.getAllBadges);
+router.put("/:badgeId", auth, authorize("admin"), badgeController.updateBadge);
+router.delete(
+  "/:badgeId",
+  auth,
+  authorize("admin"),
+  badgeController.deleteBadge
+);
+
+module.exports = router;
